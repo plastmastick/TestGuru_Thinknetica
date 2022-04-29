e08a6ae5e476dc8b5412e94dd5a536cdcee6803f
plastmastick <nick.razum23@gmail.com>
2022-04-29 10:51:21 +0300

    fix: migration

--- db/migrate/20220427192500_create_tests.rb
+++ db/migrate/20220427192500_create_tests.rb
@@ -4,9 +4,9 @@ class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
       t.string :title, null: false
-      t.integer :level, default: 1
+      t.integer :level, default: 1, null: false
       t.references :category, foreign_key: true, null: false
-      t.references :author, foreign_key: true, null: false
+      t.references :user, foreign_key: true, null: false
 
       t.timestamps
     end
--- db/migrate/20220428105750_create_answers.rb
+++ db/migrate/20220428105750_create_answers.rb
@@ -4,7 +4,7 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
       t.string :body, null: false
-      t.boolean :correct, default: false
+      t.boolean :correct, default: false, null: false
       t.references :question, foreign_key: true, null: false
 
       t.timestamps

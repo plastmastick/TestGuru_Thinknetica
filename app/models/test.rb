00fb9eb07459fb9bb3f26d217bad27f92f472b6a
plastmastick <nick.razum23@gmail.com>
2022-05-04 20:53:55 +0300

    feat: search test by category name

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  def self.tests_name_by_category(category_name)
+    categories_join = 'JOIN categories ON tests.category_id = categories.id'
+    Test.select(:title).joins(categories_join).where("categories.title = '#{category_name}'")
+  end
 end

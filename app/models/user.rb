a3d7643858e80651b41d4c56677455e9464d9de0
plastmastick <nick.razum23@gmail.com>
2022-05-04 20:41:58 +0300

    feat: add search user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  def tests_by_level(level)
+    results_join = 'JOIN results ON tests.id = results.test_id'
+    Test.joins(results_join).where("results.user_id = #{id}").and(Test.where(level: level))
+  end
 end

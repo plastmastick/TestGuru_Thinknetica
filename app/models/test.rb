dcb2e6795ecc1b6a4472221f8c3f5a8223c76768
plastmastick <nick.razum23@gmail.com>
2022-05-12 20:56:55 +0300

    fix: association options

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,7 @@
 
 class Test < ApplicationRecord
   belongs_to :category
-  belongs_to :author, class_name: "User", inverse_of: :author_tests
+  belongs_to :author, class_name: "User", inverse_of: :author_tests, optional: true
 
   has_many :questions, dependent: :destroy
   has_many :results, dependent: :destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :results, dependent: :destroy
   has_many :tests, through: :results
   has_many :author_tests, class_name: "Test", foreign_key: :author_id,
-                          dependent: nil, inverse_of: :author
+                          dependent: :nullify, inverse_of: :author
 
   def tests_by_level(search_level)
     tests.where(tests: { level: search_level })

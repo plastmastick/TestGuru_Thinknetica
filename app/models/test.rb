db0afb2150e113c5c5939c1e50a28286ab38a8e0
plastmastick <nick.razum23@gmail.com>
2022-05-26 17:42:55 +0300

    feat: add scope's in models

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,6 +8,11 @@ class Test < ApplicationRecord
   has_many :results, dependent: :destroy
   has_many :users, through: :results
 
+  scope :tests_by_level, ->(value) { where(level: value) }
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :high, -> { where(level: 5..Float::INFINITY) }
+
   def self.tests_title_by_category(category_title)
     joins(:category)
       .where(categories: { title: category_title })

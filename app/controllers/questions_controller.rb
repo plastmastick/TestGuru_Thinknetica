c816b55bd7633e15caf86b8791a899a64c497f46
plastmastick <nick.razum23@gmail.com>
2022-06-07 20:10:07 +0300

    feat: question => add rescue and fix controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,8 @@ class QuestionsController < ApplicationController
   before_action :find_test, only: %i[create]
   before_action :find_question, only: %i[show destroy]
 
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
   def index
     text = "Questions: "
     Question.all.pluck(:body).each { |q| text += "\n #{q}" }
@@ -11,15 +13,13 @@ class QuestionsController < ApplicationController
     render plain: text
   end
 
-  def show
-    # render plain: @question.inspect
-  end
+  def show; end
 
   def new; end
 
   def create
     new_question = Question.create!(test: @test, body: params[:question][:body])
-    render plain: new_question.inspect
+    redirect_to question_path(new_question) if new_question.persisted?
   end
 
   def destroy
@@ -28,15 +28,15 @@ class QuestionsController < ApplicationController
 
   private
 
-  # def question_params
-  #   params.require(:question).permit(:body)
-  # end
-
   def find_test
     @test = Test.find(params[:test_id])
   end
 
   def find_question
     @question = Question.find(params[:id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question was not found'
   end
 end

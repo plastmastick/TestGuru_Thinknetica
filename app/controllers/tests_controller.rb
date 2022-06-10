d497f04932ea61de70bb8d9b1aa5a50d462c1c38
plastmastick <nick.razum23@gmail.com>
2022-06-10 21:04:32 +0300

    feat: test views & controller create / upd

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,4 +1,57 @@
 # frozen_string_literal: true
 
 class TestsController < ApplicationController
+  before_action :find_test, only: %i[show edit update destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+
+  def index
+    @tests = Test.all
+  end
+
+  def show
+    redirect_to test_questions_path(@test)
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+    if @test.save
+      redirect_to tests_path
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def destroy
+    @test.destroy
+    redirect_to tests_path
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id)
+  end
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
+  def rescue_with_test_not_found
+    render plain: 'Test was not found'
+  end
 end

a2fa75fff6fd6bd2d2a3d77a297cb4cca34a740d
plastmastick <nick.razum23@gmail.com>
2022-06-10 22:14:03 +0300

    feat: helper github_url & footer upd

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,8 @@
 
 module ApplicationHelper
   TEST_LEVELS = { 0 => :easy, 1 => :elementary, 2 => :advanced, 3 => :hard }.freeze
+  AUTHOR = 'plastmastick'
+  REPO = 'TestGuru'
 
   def test_level(test)
     TEST_LEVELS[test.level] || :hero
@@ -9,5 +11,9 @@ module ApplicationHelper
 
   def current_year
     Time.zone.now.year
+  end
+
+  def github_url
+    "https://github.com/#{AUTHOR}/#{REPO}"
   end
 end

f2048bb80692bd1014e54d3c6b2199a8070c7047
plastmastick <nick.razum23@gmail.com>
2022-06-10 22:06:23 +0300

    feat: helper question header & views upd

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module QuestionsHelper
+  def question_header(test)
+    case controller.action_name
+    when 'edit'
+      "Edit '#{test.title}' Question"
+    when 'new'
+      "Create New '#{test.title}' Question"
+    when 'show'
+      "'#{test.title}' Question"
+    end
+  end
 end

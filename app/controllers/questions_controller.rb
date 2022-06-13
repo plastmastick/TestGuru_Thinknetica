48cd0e4a15e275fe3b88eeab70ccf4c6ae70318b
plastmastick <nick.razum23@gmail.com>
2022-06-13 21:07:11 +0300

    fix: remarks 2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -25,7 +25,7 @@ class QuestionsController < ApplicationController
 
   def destroy
     @question.destroy
-    redirect_to test_questions_path(@question.test)
+    redirect_to test_path(@question.test)
   end
 
   def update

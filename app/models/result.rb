871f7125654cde1cb58f4ffccf099cf326bfe21b
plastmastick <nick.razum23@gmail.com>
2022-06-24 20:12:05 +0300

    fix: PR remarks #2

--- app/models/result.rb
+++ app/models/result.rb
@@ -37,9 +37,7 @@ class Result < ApplicationRecord
   end
 
   def correct_answer?(answer_ids)
-    return false if answer_ids.nil?
-
-    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+    correct_answers.ids.sort == answer_ids.to_a.map(&:to_i).sort
   end
 
   def correct_answers

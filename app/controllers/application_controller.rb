22b8cc9cf610a9d16c93ecfc91804c1defc7b1f5
plastmastick <nick.razum23@gmail.com>
2022-07-23 20:31:16 +0300

    feat: add redirect Admin to admins view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    resource.is_a?(Admin) ? admin_tests_path : super
+  end
 end

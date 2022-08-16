8ca3f9dc6d41aafad609c0b6d5ef45cfc8bcd143
plastmastick <nick.razum23@gmail.com>
2022-08-16 15:09:16 +0300

    feat: locales for test & admin test

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,15 +1,21 @@
 # frozen_string_literal: true
 
 module TestsHelper
-  TEST_LEVELS = { 0 => :easy, 1 => :elementary, 2 => :advanced, 3 => :hard }.freeze
+  TEST_LEVELS = {
+    0 => :easy,
+    1 => :elementary,
+    2 => :advanced,
+    3 => :hard
+  }.freeze
 
   def test_level(test)
-    TEST_LEVELS[test.level] || :hero
+    level_name = TEST_LEVELS[test.level] || :epic
+    t("helpers.tests.levels.#{level_name}")
   end
 
   def author_name(test)
     if test.author.nil?
-      'Unknown'
+      t('helpers.tests.unknown')
     else
       test.author.full_name
     end

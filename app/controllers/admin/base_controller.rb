7a93cd1c831bf7b727570213ca50cfd2c912aa0c
plastmastick <nick.razum23@gmail.com>
2022-08-18 10:33:24 +0300

    feat: nav admin upd && fix permissions

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -8,8 +8,9 @@ class Admin::BaseController < ApplicationController
   private
 
   def admin_required!
-    unless user_signed_in? || current_user.is_a?(Admin)
-      redirect_to root_path, alert: 'You are not authorized to view this page'
-    end
+    # byebug
+    return if user_signed_in? && current_user.is_a?(Admin)
+
+    redirect_to root_path, alert: 'You are not authorized to view this page'
   end
 end

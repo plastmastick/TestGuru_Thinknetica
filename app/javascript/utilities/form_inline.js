4bf1d7e7f36c7458d90509538955d955555dfe58
plastmastick <nick.razum23@gmail.com>
2022-09-03 16:24:20 +0300

    fix: refactor

--- app/javascript/utilities/form_inline.js
+++ app/javascript/utilities/form_inline.js
@@ -1,31 +1,22 @@
 export class FormInline {
     constructor(formInlineLink) {
         this.formInlineLink = formInlineLink
+        this.testId = this.formInlineLink.dataset.testId
+        this.testTitle = document.querySelector('.test-title[data-test-id="' + this.testId + '"]')
+        this.formInline = document.querySelector('.form-inline[data-test-id="' + this.testId + '"]')
 
         this._setup()
     }
 
-    formInlineLinkHandler(event) {
-        event.preventDefault()
-
-        const testId = this.formInlineLink.dataset.testId
-
-        this.formInlineHandler(testId)
-    }
-
-    formInlineHandler(testId) {
-        const link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]')
-        const testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"]')
-        const formInline = document.querySelector('.form-inline[data-test-id="' + testId + '"]')
-
-        if(formInline.classList.contains(('hide'))) {
-            testTitle.classList.add('hide')
-            formInline.classList.remove('hide')
-            link.value = I18n.t('helpers.buttons.cancel')
+    formInlineHandler() {
+        if(this.formInline.classList.contains(('hide'))) {
+            this.testTitle.classList.add('hide')
+            this.formInline.classList.remove('hide')
+            this.formInlineLink.value = I18n.t('helpers.buttons.cancel')
         } else {
-            testTitle.classList.remove('hide')
-            formInline.classList.add('hide')
-            link.value = I18n.t('helpers.buttons.edit')
+            this.testTitle.classList.remove('hide')
+            this.formInline.classList.add('hide')
+            this.formInlineLink.value = I18n.t('helpers.buttons.edit')
         }
     }
 
@@ -37,6 +28,9 @@ export class FormInline {
             this.formInlineHandler(resourceId)
         }
 
-        this.formInlineLink.addEventListener('click', event => this.formInlineLinkHandler(event) )
+        this.formInlineLink.addEventListener('click', event => {
+            event.preventDefault()
+            this.formInlineHandler()
+        } )
     }
 }

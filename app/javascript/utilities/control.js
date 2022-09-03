e2fc63118247c6e169acca8426862198bc1fffa8
plastmastick <nick.razum23@gmail.com>
2022-09-03 18:37:45 +0300

    feat: add progress bar

--- app/javascript/utilities/control.js
+++ app/javascript/utilities/control.js
@@ -1,11 +1,13 @@
 import { TableSorting } from "./table_sorting"
 import { PasswordConfirmation } from "./password_confirmation"
 import { FormInline } from "./form_inline"
+import { ProgressBar } from "./progress_bar"
 
 document.addEventListener('turbolinks:load', function () {
     const sortTable = document.querySelector('#table-tests')
     const regForm = document.querySelector('#reg-form')
     const formsInline = document.querySelectorAll('.form-inline-link')
+    const progressBar = document.querySelector('.test-progress-bar')
 
     if (sortTable) new TableSorting(sortTable)
 
@@ -16,4 +18,6 @@ document.addEventListener('turbolinks:load', function () {
             new FormInline(formsInline[i])
         }
     }
+
+    if (progressBar) new ProgressBar(progressBar)
 })

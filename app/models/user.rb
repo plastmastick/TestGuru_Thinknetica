ce589165917fa2529e5e507641036870e1d5b134
plastmastick <nick.razum23@gmail.com>
2022-09-06 20:15:41 +0300

    fix: simple regexp for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,7 @@ class User < ApplicationRecord
   has_many :author_tests, class_name: "Test", foreign_key: :author_id,
                           dependent: :nullify, inverse_of: :author
 
-  REGEXP_EMAIL = /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+-+)|([A-Za-z0-9]+\.+)
-                  |([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+  REGEXP_EMAIL = /.+@.+\..+/i
 
   validates :first_name, presence: true
   validates :email, presence: true,

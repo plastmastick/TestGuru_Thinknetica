2aa3047aee9c8cdb45bd63b1bfd06986e7747bdf
plastmastick <nick.razum23@gmail.com>
2022-09-15 11:20:40 +0300

    fix: PR fix remarks

--- app/services/badge_assign_service.rb
+++ app/services/badge_assign_service.rb
@@ -9,40 +9,34 @@ class BadgeAssignService
   end
 
   def call
-    @badges.select do |badge|
-      if badge.option.blank?
-        send(badge.rule)
-      else
-        send(badge.rule, badge.option)
-      end
-    end
+    @badges.select { |badge| send(badge.rule, badge.option) }
   end
 
   private
 
-  def all_tests_tried_to_pass?
+  def all_tests_tried_to_pass?(option)
     Test.all.count <= @user.results.pluck(:test_id).uniq.count
   end
 
-  def all_tests_passed?
+  def all_tests_passed?(option)
     Test.all.count <= @user.results.where(passed: true).pluck(:test_id).uniq.count
   end
 
   def first_attempt?(test_title)
     test = Test.where(title: test_title).first
-    return if @test != test
+    return false if @test != test
 
     @user.results.where(test: test).order(id: :asc).first.passed == true
   end
 
   def all_tests_by_level?(level)
-    return if @test.level.to_s != level
+    return false if @test.level.to_s != level
 
     Test.by_level(level).count == @user.tests.by_level(level).uniq.count
   end
 
   def all_tests_by_category?(category)
-    return if @test.category.title != category
+    return false if @test.category.title != category
 
     Test.by_category(category).count == @user.tests.by_category(category).uniq.count
   end
